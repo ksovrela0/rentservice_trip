1ea2ecf06c2efec170408c6d9920c560c06c508c
ksovrela0 <g.ksovreli@psp.ge>
2021-09-08 13:45:33 +0400

    sadasd

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/font-awesome.css">
     <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/styles.css?v=1.2">
+    <link rel="stylesheet" href="css/styles.css?v=1.3">
     <link rel="stylesheet" href="css/mystyles.css">
     <script src="js/modernizr.js"></script>
 
