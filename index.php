90a20a5b9ecae45a5f585d2b6d527431132f725c
ksovrela0 <g.ksovreli@psp.ge>
2021-09-08 15:00:10 +0400

    fixed things

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/font-awesome.css">
     <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/styles.css?v=1.8">
-    <link rel="stylesheet" href="css/nice-select.css?v=1.7">
+    <link rel="stylesheet" href="css/styles.css?v=1.9">
+    <link rel="stylesheet" href="css/select2.css?v=1.2"/>
     <link rel="stylesheet" href="css/mystyles.css">
     <script src="js/modernizr.js"></script>
 
@@ -120,26 +120,6 @@
                         <div class="col-md-4">
                             <div class="top-user-area clearfix">
                                 <ul class="top-user-area-list list list-horizontal list-border">
-                                    <li class="top-user-area-avatar">
-                                        <a href="user-profile.html">
-                                            <img class="origin round" src="img/amaze_40x40.jpg" alt="Image Alternative text" title="AMaze" />Hi, John</a>
-                                    </li>
-                                    <li><a href="#">Sign Out</a>
-                                    </li>
-                                    <li class="nav-drop"><a href="#">USD $<i class="fa fa-angle-down"></i><i class="fa fa-angle-up"></i></a>
-                                        <ul class="list nav-drop-menu">
-                                            <li><a href="#">EUR<span class="right">€</span></a>
-                                            </li>
-                                            <li><a href="#">GBP<span class="right">£</span></a>
-                                            </li>
-                                            <li><a href="#">JPY<span class="right">円</span></a>
-                                            </li>
-                                            <li><a href="#">CAD<span class="right">$</span></a>
-                                            </li>
-                                            <li><a href="#">AUD<span class="right">A$</span></a>
-                                            </li>
-                                        </ul>
-                                    </li>
                                     <li class="top-user-area-lang nav-drop">
                                         <a href="#">
                                             <img src="img/flags/32/uk.png" alt="Image Alternative text" title="Image Title" />ENG<i class="fa fa-angle-down"></i><i class="fa fa-angle-up"></i>
@@ -147,32 +127,12 @@
                                         <ul class="list nav-drop-menu">
                                             <li>
                                                 <a title="German" href="#">
-                                                    <img src="img/flags/32/de.png" alt="Image Alternative text" title="Image Title" /><span class="right">GER</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a title="Japanise" href="#">
-                                                    <img src="img/flags/32/jp.png" alt="Image Alternative text" title="Image Title" /><span class="right">JAP</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a title="Italian" href="#">
-                                                    <img src="img/flags/32/it.png" alt="Image Alternative text" title="Image Title" /><span class="right">ITA</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a title="French" href="#">
-                                                    <img src="img/flags/32/fr.png" alt="Image Alternative text" title="Image Title" /><span class="right">FRE</span>
+                                                    <img src="img/flags/32/ge.png" alt="Image Alternative text" title="Image Title" /><span class="right">GEO</span>
                                                 </a>
                                             </li>
                                             <li>
                                                 <a title="Russian" href="#">
                                                     <img src="img/flags/32/ru.png" alt="Image Alternative text" title="Image Title" /><span class="right">RUS</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a title="Korean" href="#">
-                                                    <img src="img/flags/32/kr.png" alt="Image Alternative text" title="Image Title" /><span class="right">KOR</span>
                                                 </a>
                                             </li>
                                         </ul>
@@ -237,25 +197,16 @@
                                                         <input class="form-control" name="start" type="text" />
                                                     </div>
                                                     <select class="form-control" id="teest">
-                                                        <option selected="selected">1</option>
-                                                        <option>2</option>
-                                                        <option>3</option>
-                                                        <option>4</option>
-                                                        <option>5</option>
-                                                        <option>6</option>
-                                                        <option>7</option>
-                                                        <option>8</option>
-                                                        <option>9</option>
-                                                        <option>10</option>
-                                                        <option>11</option>
-                                                        <option>12</option>
-                                                        <option>13</option>
-                                                        <option>14</option>
+                                                        <option>რამდენი დღით ჯავშნით</option>
+                                                        <?php
+                                                            for($i = 1; $i <= 30; $i++){
+                                                                echo '<option value="'.$i.'">რამდენი დღით ჯავშნით: '.$i.'</option>';
+                                                            }
+                                                        ?>
                                                     </select>
                                                 </div>
                                                 
                                             </div>
-                                            <button class="btn btn-primary btn-lg" type="submit">შექმნა</button>
                                         </form>
                                     </div>
                                     
@@ -406,8 +357,7 @@
         <script src="js/bootstrap-datepicker.js"></script>
         <script src="js/bootstrap-timepicker.js"></script>
         <script src="js/dropit.js"></script>
-        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+        <script src="js/select2.js"></script>
         <script src="js/ionrangeslider.js"></script>
         <script src="js/icheck.js"></script>
         <script src="js/fotorama.js"></script>
