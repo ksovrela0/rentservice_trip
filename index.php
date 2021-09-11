217e130db8a21e67196f82ae442fe753ba8e0d5f
ksovrela0 <gioza-gioza@mail.ru>
2021-09-11 18:21:04 +0400

    fixed and added cars

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/font-awesome.css">
     <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/styles.css?v=2.0">
+    <link rel="stylesheet" href="css/styles.css?v=2.1">
     <link rel="stylesheet" href="css/select2.css?v=1.2"/>
     <link rel="stylesheet" href="css/mystyles.css">
     <script src="js/modernizr.js"></script>
@@ -43,68 +43,6 @@
 
 <body style="background-image: url('img/patterns/binding_dark.png');" class="boxed">
     <div class="global-wrap">
-        <div class="demo_changer" id="demo_changer">
-            <div class="demo-icon fa fa-sliders"></div>
-            <div class="form_holder">
-                <div class="line"></div>
-                <p>Color Scheme</p>
-                <div class="predefined_styles" id="styleswitch_area">
-                    <a class="styleswitch" href="index-4c392.html?default=true" style="background:#ffca18;"></a>
-                    <a class="styleswitch" href="#" data-src="bright-turquoise" style="background:#0EBCF2;"></a>
-                    <a class="styleswitch" href="#" data-src="turkish-rose" style="background:#B66672;"></a>
-                    <a class="styleswitch" href="#" data-src="salem" style="background:#12A641;"></a>
-                    <a class="styleswitch" href="#" data-src="hippie-blue" style="background:#4F96B6;"></a>
-                    <a class="styleswitch" href="#" data-src="mandy" style="background:#E45E66;"></a>
-                    <a class="styleswitch" href="#" data-src="green-smoke" style="background:#96AA66;"></a>
-                    <a class="styleswitch" href="#" data-src="horizon" style="background:#5B84AA;"></a>
-                    <a class="styleswitch" href="#" data-src="cerise" style="background:#CA2AC6;"></a>
-                    <a class="styleswitch" href="#" data-src="brick-red" style="background:#cf315a;"></a>
-                    <a class="styleswitch" href="#" data-src="de-york" style="background:#74C683;"></a>
-                    <a class="styleswitch" href="#" data-src="shamrock" style="background:#30BBB1;"></a>
-                    <a class="styleswitch" href="#" data-src="studio" style="background:#7646B8;"></a>
-                    <a class="styleswitch" href="#" data-src="leather" style="background:#966650;"></a>
-                    <a class="styleswitch" href="#" data-src="denim" style="background:#1A5AE4;"></a>
-                    <a class="styleswitch" href="#" data-src="scarlet" style="background:#FF1D13;"></a>
-                </div>
-                <div class="line"></div>
-                <p>Layout</p>
-                <div class="predefined_styles"><a class="btn btn-sm" href="#" id="btn-wide">Wide</a><a class="btn btn-sm" href="#" id="btn-boxed">Boxed</a>
-                </div>
-                <div class="line"></div>
-                <p>Background Patterns</p>
-                <div class="predefined_styles" id="patternswitch_area">
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/binding_light.png);"></a>
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/binding_dark.png);"></a>
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/dark_fish_skin.png);"></a>
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/dimension.png);"></a>
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/escheresque_ste.png);"></a>
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/food.png);"></a>
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/giftly.png);"></a>
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/grey_wash_wall.png);"></a>
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/ps_neutral.png);"></a>
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/pw_maze_black.png);"></a>
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/pw_pattern.png);"></a>
-                    <a class="patternswitch" href="#" style="background-image: url(img/patterns/simple_dashed.png);"></a>
-                </div>
-                <div class="line"></div>
-                <p>Background Images</p>
-                <div class="predefined_styles" id="bgimageswitch_area">
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/bike.jpg);" data-src="img/backgrounds/bike.jpg"></a>
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/flowers.jpg);" data-src="img/backgrounds/flowers.jpg"></a>
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/wood.jpg);" data-src="img/backgrounds/wood.jpg"></a>
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/taxi.jpg);" data-src="img/backgrounds/taxi.jpg"></a>
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/phone.jpg);" data-src="img/backgrounds/phone.jpg"></a>
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/road.jpg);" data-src="img/backgrounds/road.jpg"></a>
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/keyboard.jpg);" data-src="img/backgrounds/keyboard.jpg"></a>
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/beach.jpg);" data-src="img/backgrounds/beach.jpg"></a>
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/street.jpg);" data-src="img/backgrounds/street.jpg"></a>
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/nature.jpg);" data-src="img/backgrounds/nature.jpg"></a>
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/bridge.jpg);" data-src="img/backgrounds/bridge.jpg"></a>
-                    <a class="bgimageswitch" href="#" style="background-image: url(img/switcher/cameras.jpg);" data-src="img/backgrounds/cameras.jpg"></a>
-                </div>
-                <div class="line"></div>
-            </div>
-        </div>
         <header id="main-header">
             <div class="header-top">
                 <div class="container">
@@ -296,7 +234,60 @@
                     </div>
                     
                 </div>
-            </di?>
+            </div>
+            <div class="gap gap-small"></div>
+        </div>
+
+        <div class="container">
+            <div class="gap"></div>
+            <h3 class="text-center mb20">ფასი მოიცავს მგზავრობის სრულ ღირებულებას (და არა ერთი მგზავრის საფასურს)</h3>
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <div class="car_dest">
+                        <div class="row">
+                            <div class="col-12 col-lg-5">
+                                <div class="car_img_wrapper">
+                                    <img class="car_img_thumb" src="https://startrip.ge/timthumb/thumb.php?src=/upload/3XfjaNXCoasrGuMvrtXz9IlCMjfKPl.jpg&w=400&h=300&zc=1&q=70" />
+                                    <p class="car_img_title">TOYOTA PRIUS</p>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-7">
+                                sadasdas
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="car_dest">
+                        <div class="row">
+                            <div class="col-12 col-lg-5">
+                                <div class="car_img_wrapper">
+                                    <img class="car_img_thumb" src="https://startrip.ge/timthumb/thumb.php?src=/upload/OugMImVfH2xMi0KH8aJb3SUOEJDLva.jpg&w=400&h=300&zc=1&q=70" />
+                                    <p class="car_img_title">TOYOTA PRIUS</p>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-7">
+                                sadasdas
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="car_dest">
+                        <div class="row">
+                            <div class="col-12 col-lg-5">
+                                <div class="car_img_wrapper">
+                                    <img class="car_img_thumb" src="https://startrip.ge/timthumb/thumb.php?src=/upload/t6KTqW2Zc77zws1b5VEKE5MSeMyISc.jpg&w=400&h=300&zc=1&q=70" />
+                                    <p class="car_img_title">FORD FUSION</p>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-7">
+                                sadasdas
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="gap gap-small"></div>
         </div>
 
